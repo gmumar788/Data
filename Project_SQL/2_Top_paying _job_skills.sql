5b661562eebba9fd11a05fc1e96af6b04d2fd867
Umar Ali <g.m.umar788@example.com>
2024-11-21 21:05:22 +1100

    3rd commit

--- Project_SQL/2_Top_paying _job_skills.sql
+++ Project_SQL/2_Top_paying _job_skills.sql
@@ -17,7 +17,7 @@ WITH top_paying_jobs AS (
     LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
     WHERE
         job_title_short = 'Data Analyst' AND 
-        job_location = 'Anywhere' AND 
+        job_location IN ('Anywhere','Australia') AND 
         salary_year_avg IS NOT NULL
     ORDER BY
         salary_year_avg DESC
