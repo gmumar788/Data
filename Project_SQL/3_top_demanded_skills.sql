d017ffd1d6e31d071a4f7aab4a98e01a3598e677
Umar Ali <g.m.umar788@example.com>
2024-11-21 22:08:58 +1100

    4th comit

--- Project_SQL/3_top_demanded_skills.sql
+++ Project_SQL/3_top_demanded_skills.sql
@@ -1,5 +1,5 @@
 /*
-Question: What are the most in-demand skills for data analysts?
+Question: What are the most in-demand skills for data analysts In Australia?
 - Join job postings to inner join table similar to query 2
 - Identify the top 5 in-demand skills for a data analyst.
 - Focus on all job postings.
@@ -8,4 +8,15 @@ Question: What are the most in-demand skills for data analysts?
 */
 
 
-SELECT
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS Demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_location = 'Australia' AND job_title_short = 'Data Analyst'
+GROUP BY
+    skills
+ORDER BY Demand_count DESC
+LIMIT 5;
